21699819918d5bd64cf740e8543a39b1bcc1a8eb
sunnylqm <sunnylqm@qq.com>
2020-05-27 11:49:04 +0800

    Update quota

--- site/src/components/pricing/index.jsx
+++ site/src/components/pricing/index.jsx
@@ -34,7 +34,7 @@ function Pricing() {
                 <li className="pricing__feature">可创建3个应用，每个应用限*：</li>
                 <li className="pricing__feature">30个原生包，每个最大30M</li>
                 <li className="pricing__feature">30个热更包，每个最大3M</li>
-                <li className="pricing__feature">每日下载更新最多3千次</li>
+                <li className="pricing__feature">每日前3千次更新满速下载</li>
               </ul>
               <Button
                 shape="round"
@@ -59,8 +59,8 @@ function Pricing() {
               <ul className="pricing__feature-list">
                 <li className="pricing__feature">可创建5个应用，每个应用限*：</li>
                 <li className="pricing__feature">50个原生包，每个最大100M</li>
-                <li className="pricing__feature">50个热更包，每个最大5M</li>
-                <li className="pricing__feature">每日下载更新最多5万次</li>
+                <li className="pricing__feature">50个热更包，每个最大15M</li>
+                <li className="pricing__feature">每日前1万次更新满速下载</li>
                 <li className="pricing__feature">提供专人技术支持</li>
               </ul>
               <Button shape="round" type="primary" onClick={gotoPurchase}>
@@ -82,8 +82,8 @@ function Pricing() {
               <ul className="pricing__feature-list">
                 <li className="pricing__feature">可创建10个应用，每个应用限*：</li>
                 <li className="pricing__feature">100个原生包，每个最大500M</li>
-                <li className="pricing__feature">100个热更包，每个最大10M</li>
-                <li className="pricing__feature">每日下载更新最多10万次</li>
+                <li className="pricing__feature">100个热更包，每个最大50M</li>
+                <li className="pricing__feature">每日前10万次更新满速下载</li>
                 <li className="pricing__feature">提供专人技术支持</li>
               </ul>
               <Button shape="round" type="primary" onClick={gotoPurchase}>
