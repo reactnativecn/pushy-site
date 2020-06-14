d91ecee5e10ed747c53d4c7d99b67f9bee9331ea
sunnylqm <sunnylqm@qq.com>
2020-06-14 22:26:32 +0800

    Remove antdesignpro variables

--- admin/jest.config.js
+++ admin/jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
   testEnvironment: './tests/PuppeteerEnvironment',
   verbose: false,
   globals: {
-    ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: false,
     localStorage: null,
   },
 };
--- admin/src/components/Footer/index.tsx
+++ admin/src/components/Footer/index.tsx
@@ -4,24 +4,24 @@ import { DefaultFooter } from '@ant-design/pro-layout';
 
 export default () => (
   <DefaultFooter
-    copyright="2019 蚂蚁金服体验技术部出品"
+    copyright="2020 武汉青罗网络科技有限公司"
     links={[
       {
-        key: 'Pushy热更新后台',
-        title: 'Pushy热更新后台',
-        href: 'https://pro.ant.design',
+        key: 'Pushy热更新',
+        title: 'Pushy热更新',
+        href: 'https://pushy.reactnative.cn',
         blankTarget: true,
       },
       {
         key: 'github',
         title: <GithubOutlined />,
-        href: 'https://github.com/ant-design/ant-design-pro',
+        href: 'https://github.com/reactnativecn/react-native-pushy',
         blankTarget: true,
       },
       {
-        key: 'Ant Design',
-        title: 'Ant Design',
-        href: 'https://ant.design',
+        key: 'React Native 中文网',
+        title: 'React Native 中文网',
+        href: 'https://reactnative.cn',
         blankTarget: true,
       },
     ]}
