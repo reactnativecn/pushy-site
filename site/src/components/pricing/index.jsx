01e6fc55d5c89f0226339a1de32b579e11b168e8
sunnylqm <sunnylqm@gmail.com>
2022-07-26 13:55:53 +0800

    chore: add tooltip

--- site/src/components/pricing/index.jsx
+++ site/src/components/pricing/index.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { Button } from 'antd';
+import { Button, Tooltip } from 'antd';
 import { SmileOutlined, CarOutlined, RocketOutlined, SendOutlined } from '@ant-design/icons';
 import './pricing.css';
 
 function gotoPurchase() {
   window.location.href = 'https://update.reactnative.cn/profile';
 }
+
+const AppText = () => <Tooltip title="iOS 和 Android 版本记做不同的应用。您可删除已不再使用的应用来有效利用配额。">应用</Tooltip>;
+const PackageText = () => <Tooltip title="原生包指完整的 apk 或 ipa 格式的安装包。您可删除已不再使用的原生包来有效利用配额。">原生包</Tooltip>;
+const HotUpdateText = () => (
+  <Tooltip
+    title={`热更包指"pushy bundle"命令生成的 ppk 文件（其中包含jsbundle、图片等业务逻辑文件）。
+注意这不是用户实际下载的文件，用户实际下载的是经过服务器运算后生成的极小差量补丁包。您可删除已不再使用的热更包来有效利用配额。`}
+  >
+    热更包
+  </Tooltip>
+);
 
 function Pricing() {
   return (
@@ -23,7 +34,12 @@ function Pricing() {
               textAlign: 'center',
             }}
           >
-            新注册用户将自动获得 7 天的<u>专业版</u>免费试用评估。到期后转为免费版。
+            新注册用户将自动获得{' '}
+            <Tooltip title="您可将注册用户名、公司（或个人）名称发送至<hi@charmlot.com>，申请延长评估时间">
+              7 天
+            </Tooltip>
+            的<u>专业版</u>
+            免费试用评估。到期后转为免费版。
           </p>
           <div className="pricing pricing--rabten">
             <div className="pricing__item">
@@ -39,9 +55,21 @@ function Pricing() {
                 </span>
               </div>
               <ul className="pricing__feature-list">
-                <li className="pricing__feature">可创建3个应用，每个限*：</li>
-                <li className="pricing__feature">30个原生包，每个最大30M</li>
-                <li className="pricing__feature">30个热更包，每个最大3M</li>
+                <li className="pricing__feature">
+                  可创建3个
+                  <AppText />
+                  ，每个限*：
+                </li>
+                <li className="pricing__feature">
+                  30个
+                  <PackageText />
+                  ，每个最大30M
+                </li>
+                <li className="pricing__feature">
+                  30个
+                  <HotUpdateText />
+                  ，每个最大3M
+                </li>
               </ul>
               <Button
                 shape="round"
@@ -64,9 +92,19 @@ function Pricing() {
                 </span>
               </div>
               <ul className="pricing__feature-list">
-                <li className="pricing__feature">可创建5个应用，每个限*：</li>
-                <li className="pricing__feature">50个原生包，每个最大100M</li>
-                <li className="pricing__feature">50个热更包，每个最大15M</li>
+                <li className="pricing__feature">
+                  可创建5个
+                  <AppText /> ，每个限*：
+                </li>
+                <li className="pricing__feature">
+                  50个
+                  <PackageText />
+                  ，每个最大100M
+                </li>
+                <li className="pricing__feature">
+                  50个 <HotUpdateText />
+                  ，每个最大15M
+                </li>
                 <li className="pricing__feature">提供专人技术支持</li>
               </ul>
               <Button
@@ -91,9 +129,20 @@ function Pricing() {
                 </span>
               </div>
               <ul className="pricing__feature-list">
-                <li className="pricing__feature">可创建6个应用，每个限*：</li>
-                <li className="pricing__feature">60个原生包，每个最大300M</li>
-                <li className="pricing__feature">60个热更包，每个最大50M</li>
+                <li className="pricing__feature">
+                  可创建6个
+                  <AppText />
+                  ，每个限*：
+                </li>
+                <li className="pricing__feature">
+                  60个
+                  <PackageText />
+                  ，每个最大300M
+                </li>
+                <li className="pricing__feature">
+                  60个 <HotUpdateText />
+                  ，每个最大50M
+                </li>
                 <li className="pricing__feature">提供专人技术支持</li>
               </ul>
               <Button
@@ -118,9 +167,20 @@ function Pricing() {
                 </span>
               </div>
               <ul className="pricing__feature-list">
-                <li className="pricing__feature">可创建10个应用，每个限*：</li>
-                <li className="pricing__feature">100个原生包，每个最大1000M</li>
-                <li className="pricing__feature">100个热更包，每个最大200M</li>
+                <li className="pricing__feature">
+                  可创建10个
+                  <AppText />
+                  ，每个限*：
+                </li>
+                <li className="pricing__feature">
+                  100个
+                  <PackageText />
+                  ，每个最大1000M
+                </li>
+                <li className="pricing__feature">
+                  100个 <HotUpdateText />
+                  ，每个最大200M
+                </li>
                 <li className="pricing__feature">提供专人技术支持</li>
               </ul>
               <Button
@@ -139,7 +199,7 @@ function Pricing() {
               <a href="mailto:hi@charmlot.com">hi@charmlot.com</a> 我们将第一时间回复。
             </p>
             <p>
-              *注：iOS和Android版本记做不同的应用。
+              *注：iOS 和 Android 版本记做不同的应用。
               <br />
               原生包指完整的apk/ipa安装包。热更包指pushy
               bundle命令生成的ppk文件（不是用户实际下载的增量更新文件）。
