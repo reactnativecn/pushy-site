66b4ad72aaa8669a8aa177d45faad50027610c36
sunnylqm <sunnylqm@gmail.com>
2026-02-23 13:04:55 +0800

    specify out dir

--- site/rspress.config.ts
+++ site/rspress.config.ts
@@ -4,6 +4,7 @@ import { pluginSass } from '@rsbuild/plugin-sass';
 import rspressPluginMermaid from 'rspress-plugin-mermaid';
 
 export default defineConfig({
+  outDir: 'out',
   root: path.join(__dirname, 'pages'),
   title: 'Pushy 极速热更新',
   description: 'Pushy 极速热更新',
