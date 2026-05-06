5fb2b63794b2a76130a372588b4d56476f994ae5
sunnylqm <sunnylqm@users.noreply.github.com>
2026-05-06 04:18:12 +0000

    deploy: 4a613f211bef2ff562bf94ba1b8b24b98690e4bd

--- static/js/lib-router.533e3da9e3.js.LICENSE.txt
+++ static/js/lib-router.533e3da9e3.js.LICENSE.txt
@@ -1,5 +1,5 @@
 /**
- * react-router v7.14.2
+ * react-router v7.15.0
  *
  * Copyright (c) Remix Software Inc.
  *
---
+++ sw.js
@@ -0,0 +1,68 @@
+const CACHE_NAME = 'pushy-site-v1';
+const PRECACHE_URLS = ['/', '/manifest.webmanifest', '/images/logo.svg'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(PRECACHE_URLS))
+      .then(() => self.skipWaiting()),
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key !== CACHE_NAME)
+            .map((key) => caches.delete(key)),
+        ),
+      )
+      .then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+
+  if (request.method !== 'GET') {
+    return;
+  }
+
+  const url = new URL(request.url);
+  if (url.origin !== self.location.origin) {
+    return;
+  }
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(request, copy));
+          return response;
+        })
+        .catch(() => caches.match(request).then((cached) => cached || caches.match('/'))),
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cached) => {
+      if (cached) {
+        return cached;
+      }
+
+      return fetch(request).then((response) => {
+        if (response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(request, copy));
+        }
+        return response;
+      });
+    }),
+  );
+});
